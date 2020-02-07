9eff5579ca1eb857c16afaf1e6968763acf8bd69
Nolin Reddy <nolinr@bbd.co.za>
2020-02-07 16:42:16 +0200

    it works

--- src/main/java/com/example/javafx/JavafxApplication.java
+++ src/main/java/com/example/javafx/JavafxApplication.java
@@ -15,41 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavafxApplication extends JFrame {
 
 	public JavafxApplication() {
-
-		initUI();
-	}
-
-	private void initUI() {
-
-		var quitButton = new JButton("Quit");
-
-		quitButton.addActionListener((ActionEvent event) -> {
-			System.exit(0);
-		});
-
-		createLayout(quitButton);
-
-		setTitle("Quit button");
-		setSize(300, 200);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-
-	private void createLayout(JComponent... arg) {
-
-		var pane = getContentPane();
-		var gl = new GroupLayout(pane);
-		pane.setLayout(gl);
-
-		gl.setAutoCreateContainerGaps(true);
-
-		gl.setHorizontalGroup(gl.createSequentialGroup()
-				.addComponent(arg[0])
-		);
-
-		gl.setVerticalGroup(gl.createSequentialGroup()
-				.addComponent(arg[0])
-		);
+		new Window();
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/javafx/Window.java
@@ -0,0 +1,35 @@
+package com.example.javafx;
+
+import javax.swing.JFrame;
+
+public class Window {
+	
+	public static final int WIDTH = 307, HEIGHT = 630;
+	private JFrame window;
+	private Board board;
+	
+	public Window(){
+		window = new JFrame("Tetris Game");
+		window.setSize(WIDTH, HEIGHT);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setLocationRelativeTo(null);
+		
+		board = new Board();
+		
+		
+		
+		window.add(board);
+		window.addKeyListener(board);
+		
+		
+		window.setVisible(true);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		new Window();
+	}
+
+}
